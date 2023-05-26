1991e023671b40db524167926af41e452c32ee05
Snowy Snowtime <47854261+SnowySnowtime@users.noreply.github.com>
2023-05-25 18:29:30 -0700

    more cheese code to make needler more awesome

--- lua/weapons/arccw_snow_ce_needler.lua
+++ lua/weapons/arccw_snow_ce_needler.lua
@@ -558,11 +558,10 @@ function SWEP:Think()
     if CLIENT and (game.SinglePlayer() or IsFirstTimePredicted()) then
         self:ProcessRecoil()
     end
-
+	local param = self:Clip1() + 1
+	self:SetPoseParameter( "ammo", param )
     if CLIENT and IsValid(vm) then
-		local param = self:Clip1() + 1
 		vm:SetPoseParameter( "ammo", param )
-		print(param)
 
         for i = 1, vm:GetBoneCount() do
             vm:ManipulateBoneScale(i, vec1)
@@ -687,4 +686,178 @@ function SWEP:Think()
     if self:GetUBGLDebounce() and !self:GetOwner():KeyDown(IN_RELOAD) then
         self:SetUBGLDebounce( false )
     end
+end
+
+function SWEP:Reload()
+    if IsValid(self:GetHolster_Entity()) then return end
+    if self:GetHolster_Time() > 0 then return end
+
+    if self:GetOwner():IsNPC() then
+        return
+    end
+
+    if self:GetState() == ArcCW.STATE_CUSTOMIZE then
+        return
+    end
+
+    -- Switch to UBGL
+    if self:GetBuff_Override("UBGL") and self:GetOwner():KeyDown(IN_USE) then
+        if self:GetInUBGL() then
+            --net.Start("arccw_ubgl")
+            --net.WriteBool(false)
+            --net.SendToServer()
+
+            self:DeselectUBGL()
+        else
+            --net.Start("arccw_ubgl")
+            --net.WriteBool(true)
+            --net.SendToServer()
+
+            self:SelectUBGL()
+        end
+
+        return
+    end
+
+    if self:GetInUBGL() then
+        if self:GetNextSecondaryFire() > CurTime() then return end
+            self:ReloadUBGL()
+        return
+    end
+
+    if self:GetNextPrimaryFire() >= CurTime() then return end
+    -- if !game.SinglePlayer() and !IsFirstTimePredicted() then return end
+
+
+    if self.Throwing then return end
+    if self.PrimaryBash then return end
+
+    -- with the lite 3D HUD, you may want to check your ammo without reloading
+    local Lite3DHUD = self:GetOwner():GetInfo("arccw_hud_3dfun") == "1"
+    if self:GetOwner():KeyDown(IN_WALK) and Lite3DHUD then
+        return
+    end
+
+    if self:GetMalfunctionJam() then
+        local r = self:MalfunctionClear()
+        if r then return end
+    end
+
+    if !self:GetMalfunctionJam() and self:Ammo1() <= 0 and !self:HasInfiniteAmmo() then return end
+
+    if self:HasBottomlessClip() then return end
+
+    if self:GetBuff_Hook("Hook_PreReload") then return end
+
+    -- if we must dump our clip when reloading, our reserve ammo should be more than our clip
+    local dumpclip = self:GetBuff_Hook("Hook_ReloadDumpClip")
+    if dumpclip and !self:HasInfiniteAmmo() and self:Clip1() >= self:Ammo1() then
+        return
+    end
+
+    self.LastClip1 = self:Clip1()
+
+    local reserve = self:Ammo1()
+
+    reserve = reserve + self:Clip1()
+    if self:HasInfiniteAmmo() then reserve = self:GetCapacity() + self:Clip1() end
+
+    local clip = self:GetCapacity()
+
+    local chamber = math.Clamp(self:Clip1(), 0, self:GetChamberSize())
+    if self:GetNeedCycle() then chamber = 0 end
+
+    local load = math.Clamp(clip + chamber, 0, reserve)
+
+    if !self:GetMalfunctionJam() and load <= self:Clip1() then return end
+
+    self:SetBurstCount(0)
+
+    local shouldshotgunreload = self:GetBuff_Override("Override_ShotgunReload")
+    local shouldhybridreload = self:GetBuff_Override("Override_HybridReload")
+
+    if shouldshotgunreload == nil then shouldshotgunreload = self.ShotgunReload end
+    if shouldhybridreload == nil then shouldhybridreload = self.HybridReload end
+
+    if shouldhybridreload then
+        shouldshotgunreload = self:Clip1() != 0
+    end
+
+    if shouldshotgunreload and self:GetShotgunReloading() > 0 then return end
+
+    local mult = self:GetBuff_Mult("Mult_ReloadTime")
+
+    if shouldshotgunreload then
+        local anim = "sgreload_start"
+        local insertcount = 0
+
+        local empty = self:Clip1() == 0 --or self:GetNeedCycle()
+
+        if self.Animations.sgreload_start_empty and empty then
+            anim = "sgreload_start_empty"
+            empty = false
+            if (self.Animations.sgreload_start_empty or {}).ForceEmpty == true then
+                empty = true
+            end
+
+            insertcount = (self.Animations.sgreload_start_empty or {}).RestoreAmmo or 1
+        else
+            insertcount = (self.Animations.sgreload_start or {}).RestoreAmmo or 0
+        end
+
+        anim = self:GetBuff_Hook("Hook_SelectReloadAnimation", anim) or anim
+
+        local time = self:GetAnimKeyTime(anim)
+        local time2 = self:GetAnimKeyTime(anim, true)
+
+        if time2 >= time then
+            time2 = 0
+        end
+
+        if insertcount > 0 then
+            self:SetMagUpCount(insertcount)
+            self:SetMagUpIn(CurTime() + time2 * mult)
+        end
+        self:PlayAnimation(anim, mult, true, 0, true, nil, true)
+
+        self:SetReloading(CurTime() + time * mult)
+
+        self:SetShotgunReloading(empty and 4 or 2)
+    else
+        local anim = self:SelectReloadAnimation()
+
+        if !self.Animations[anim] then print("Invalid animation \"" .. anim .. "\"") return end
+
+        self:PlayAnimation(anim, mult, true, 0, false, nil, true)
+
+        local reloadtime = self:GetAnimKeyTime(anim, true) * mult
+        local reloadtime2 = self:GetAnimKeyTime(anim, false) * mult
+
+        self:SetNextPrimaryFire(CurTime() + reloadtime2)
+        self:SetReloading(CurTime() + reloadtime2)
+
+        self:SetMagUpCount(0)
+        self:SetMagUpIn(CurTime() + reloadtime - 1)
+    end
+
+    self:SetClipInfo(load)
+    if game.SinglePlayer() then
+        self:CallOnClient("SetClipInfo", tostring(load))
+    end
+
+    for i, k in pairs(self.Attachments) do
+        if !k.Installed then continue end
+        local atttbl = ArcCW.AttachmentTable[k.Installed]
+
+        if atttbl.DamageOnReload then
+            self:DamageAttachment(i, atttbl.DamageOnReload)
+        end
+    end
+
+    if !self.ReloadInSights then
+        self:ExitSights()
+        self.Sighted = false
+    end
+
+    self:GetBuff_Hook("Hook_PostReload")
 end
